589a9ae93b4057b55eef292e3702915601dd8572
Sulojan <sulojanrajkumar@gmail.com>
2026-09-19 01:13:55 +0530

    feat: Started Gemma model by BU approach

---
+++ model_gemma.py
@@ -0,0 +1,9 @@
+import math
+from typing import List, Optional, Tuple
+
+import torch
+from torch import nn
+from torch.nn import CrossEntropyLoss
+
+from model_siglip import SiglipVisionConfig, SiglipVisionModel
+
